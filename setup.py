f87370c340e07e506ba08d0c1a1c8f78f1906da3
Xavier Garrido <xavier.garrido@gmail.com>
2019-12-26 18:12:54 +0100

    add more supported python versions

--- setup.py
+++ setup.py
@@ -28,6 +28,8 @@ setup(
         "Natural Language :: English",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6"
+        "Programming Language :: Python :: 3.7"
+        "Programming Language :: Python :: 3.8"
         ],
     entry_points={},
     ext_modules=[mcm, cov],
