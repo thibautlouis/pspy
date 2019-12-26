5a4082ebdb8566734c9058d39b5dd49dfb8d28d8
Xavier Garrido <xavier.garrido@gmail.com>
2019-12-26 17:47:53 +0100

    improve setup.py by adding missing requirements

--- setup.py
+++ setup.py
@@ -1,54 +1,44 @@
-#- 
-# setup.py
-#-
-
 import setuptools
-from distutils.errors import DistutilsError
-from numpy.distutils.core import setup, Extension, build_ext, build_src
-import os, sys
-import subprocess as sp
-import numpy as np
-
+from numpy.distutils.core import setup
+from numpy.distutils.extension import Extension
 
 compile_opts = {
-    'extra_f90_compile_args': ['-fopenmp','-ffree-line-length-none', '-fdiagnostics-color=always', '-Wno-tabs'],
-    'f2py_options': ['skip:', 'map_border', 'calc_weights', ':'],
-    'extra_link_args': ['-fopenmp']
+    "extra_f90_compile_args": [
+        "-fopenmp", "-ffree-line-length-none", "-fdiagnostics-color=always", "-Wno-tabs"],
+    "f2py_options": ["skip:", "map_border", "calc_weights"],
+    "extra_link_args": ["-fopenmp"]
 }
 
-mcm = Extension(name = 'pspy.mcm_fortran.mcm_fortran',
-                sources = ['pspy/mcm_fortran/mcm_fortran.f90', 'pspy/wigner3j/wigner3j_sub.f'],
+mcm = Extension(name="pspy.mcm_fortran.mcm_fortran",
+                sources=["pspy/mcm_fortran/mcm_fortran.f90", "pspy/wigner3j/wigner3j_sub.f"],
                 **compile_opts)
-cov =Extension(name = 'pspy.cov_fortran.cov_fortran',
-               sources = ['pspy/cov_fortran/cov_fortran.f90', 'pspy/wigner3j/wigner3j_sub.f'],
-               **compile_opts)
-
-requirements=['numpy','pixell']
+cov = Extension(name="pspy.cov_fortran.cov_fortran",
+                sources=["pspy/cov_fortran/cov_fortran.f90", "pspy/wigner3j/wigner3j_sub.f"],
+                **compile_opts)
 
 setup(
-      author="Simons Observatory Collaboration Power Spectrum Task Force",
-      author_email='',
-      classifiers=[
-                   'Development Status :: 2 - Pre-Alpha',
-                   'Intended Audience :: Developers',
-                   'License :: OSI Approved :: BSD License',
-                   'Natural Language :: English',
-                   'Programming Language :: Python :: 3.5',
-                   'Programming Language :: Python :: 3.6',
-                   ],
-      description="PSPY",
-      url= "https://github.com/thibautlouis/pspy",
-      download_url= "https://github.com/thibautlouis/pspy",
-      package_dir={"pspy": "pspy"},
-      entry_points={},
-      ext_modules=[mcm,cov],
-      install_requires=requirements,
-      license="BSD license",
-      packages=['pspy'],
-      data_files=[("data", ["data/Planck_Parchment_RGB.txt"])],
-      name= "pspy",
-      version= '0.0.1'
+    name="pspy",
+    version= "0.0.1",
+    author="Simons Observatory Collaboration Power Spectrum Task Force",
+    author_email="",
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6"
+        ],
+    entry_points={},
+    ext_modules=[mcm, cov],
+    install_requires=[
+        "numpy",
+        "healpy",
+        "cython", # this one should be installed by pyFFTW
+        "pyFFTW @ git+https://github.com/pyFFTW/pyFFTW.git",
+        "pillow", # this one should be installed by pixell
+        "pixell @ git+https://github.com/simonsobs/pixell.git"],
+    license="BSD license",
+    packages=["pspy"],
+    data_files=[("data", ["data/Planck_Parchment_RGB.txt"])],
 )
-
-
-
