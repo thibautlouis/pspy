5ce131afa8aeb6d397550971c01702706388ec73
Thibaut <thbtlouis@gmail.com>
2019-12-28 15:31:16 +0100

    pfff

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -21,7 +21,7 @@ class Mock(MagicMock):
     def __getattr__(cls, name):
         return MagicMock()
 
-MOCK_MODULES = ['pixell','pspy.mcm_fortran','pspy.cov_fortran' ]
+MOCK_MODULES = ['pixell','pspy.mcm_fortran','pspy.cov_fortran','scipy.fftpack' ]
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 
