96e8444c45e256ad100a058315293a88dc9e310c
Thibaut <thbtlouis@gmail.com>
2019-12-28 12:23:01 +0100

    try to fix readthedoc issue

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -21,7 +21,7 @@ class Mock(MagicMock):
     def __getattr__(cls, name):
         return MagicMock()
 
-MOCK_MODULES = ['pixell']
+MOCK_MODULES = ['pixell','healpy']
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 
