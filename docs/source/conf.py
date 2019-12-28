d5ab136dbbddf801413f985e7e0da9924f951531
Thibaut <thbtlouis@gmail.com>
2019-12-28 15:23:10 +0100

    add fortran lib as mock

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -44,6 +44,10 @@ extensions = ['sphinx.ext.autodoc']
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
+# Present auto-documented members in source order (rather than alphabetical).
+autodoc_member_order = 'bysource'
+
+
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
