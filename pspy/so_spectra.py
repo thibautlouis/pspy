0f21d53d3be15e0638fbf5b11aaa49114e9acdb0
Thibaut <thbtlouis@gmail.com>
2019-12-28 10:55:07 +0100

    format doc

--- pspy/so_spectra.py
+++ pspy/so_spectra.py
@@ -1,22 +1,30 @@
 """
-@brief: python routines for power spectra estimation and debiasing.
+Routines for power spectra estimation and debiasing.
 """
-from __future__ import absolute_import, print_function
 import healpy as hp, numpy as np
 from pspy import pspy_utils,so_mcm
 
 def get_spectra(alm1,alm2=None,spectra=None):
-    """
-    @brief get the power spectrum of alm1 and alm2, we use healpy.alm2cl for doing this.
+    
+    """Get the power spectrum of alm1 and alm2, we use healpy.alm2cl for doing this.
     for the spin0 and spin2 case it is a bit ugly as we have to deal with healpix convention.
     Our  convention for spectra is:  ['TT','TE','TB','ET','BT','EE','EB','BE','BB']
-    or a corresponding convention for spin0 and spin2 fields (replacing T by gal and E by kappa for example).
     while healpix convention is to take alm1,alm2 and return ['TT','EE','BB','TE','EB','TB']
-    @param alm1 the spherical harmonic transform of map1
-    @param (optional) alm2 the spherical harmonic transform of map2
-    @param (optional) spectra,  needed for spin0 and spin2 cross correlation, the arrangement of the spectra
-    @return l the multipole
-    @return cl the 1d array power spectrum or cl_dict (for spin0 and spin2) a dictionnary of cl with entry spectra
+    
+    Parameters
+    ----------
+    alm1: 1d array
+      the spherical harmonic transform of map1
+    alm2: 1d array
+      the spherical harmonic transform of map2
+    spectra: list of strings
+      needed for spin0 and spin2 cross correlation, the arrangement of the spectra
+      
+    Return
+    ----------
+
+    The function returns the multipole array l and cl a 1d power spectrum array or
+    cl_dict (for spin0 and spin2) a dictionnary of cl with entry spectra
     """
 
     if spectra is None:
@@ -30,51 +38,72 @@ def get_spectra(alm1,alm2=None,spectra=None):
         cl_dict={}
         cls=hp.sphtfunc.alm2cl(alm1,alm2)
         l=np.arange(len(cls[0]))
+        """ spectra_healpix=[TT,EE,BB,TE,EB,TB] """
         spectra_healpix=[spectra[0],spectra[5],spectra[8],spectra[1],spectra[6],spectra[2]]
         for c,f in enumerate(spectra_healpix):
             cl_dict[f]=cls[c]
 
         if alm2 is None:
+            #here we set ET=TE, BE=EB and BT=TB
             cl_dict[spectra[3]]= cl_dict[spectra[1]]
             cl_dict[spectra[7]]= cl_dict[spectra[6]]
             cl_dict[spectra[4]]= cl_dict[spectra[2]]
         else:
             #here we need to recompute cls inverting the order of the alm to get ET,BT and BE
             cls=hp.sphtfunc.alm2cl(alm2,alm1)
+            """ spectra_healpix=[TT,EE,BB,ET,BE,BT] """
             spectra_healpix=[spectra[0],spectra[5],spectra[8],spectra[3],spectra[7],spectra[4]]
             for c,f in enumerate(spectra_healpix):
                 cl_dict[f]=cls[c]
-
 
         return l,cl_dict
 
 def bin_spectra(l,cl,binning_file,lmax,type,spectra=None,mbb_inv=None,mcm_inv=None):
     
-    """
-    @brief bin the power spectra
-    @param l: the multipole
-    @param cl: the power spectra to bin, can be a 1d array (spin0) or a dictionnary (spin0 and spin2)
-    @param binning_file: a binning file with format bin low, bin high, bin mean
-    @param lmax: the maximum multipole to consider
-    @param type: the type of binning, either bin Cl or bin Dl
-    @param (optional) spectra: needed for spin0 and spin2 cross correlation, the arrangement of the spectra
-    @param (optional) mbb_inv: the inverse of the mode coupling matrix to debiais the spectra
-    @return bin_c: 1d array with the center of the bin
-    @return binnedPower: 1d array with the binned power spectra
+    """Bin the power spectra according to a binning file and optionnaly deconvolve the mode coupling matrix
+    
+    Parameters
+    ----------
+    l: 1d array
+      the multipoles
+    cl: 1d array or dict of 1d array
+      the power spectra to bin, can be a 1d array (spin0) or a dictionnary (spin0 and spin2)
+    binning_file: data file
+      a binning file with format bin low, bin high, bin mean
+    lmax: int
+      the maximum multipole to consider
+    type: string
+      the type of binning, either bin Cl or bin Dl
+    spectra: list of string
+      needed for spin0 and spin2 cross correlation, the arrangement of the spectra
+    mbb_inv: 2d array
+      the inverse of the binned mode coupling matrix to debiais the spectra
+    mcm_inv: 2d array
+      the inverse of the raw mode coupling matrix to debiais the spectra
+      note that you have to choose between debiasing with the binned or the
+      raw matrix
+      
+    Return
+    ----------
+      
+    The function return the binned multipole array bin_c and a 1d power spectrum
+    array (or dictionnary of 1d power spectra if spectra is not None).
     """
     
+    #You don't want to deconvolve the binned mcm and the unbinned mcm
+    #You have to choose one of the other
+    #TODO: rework this function logic
     
     if mbb_inv is not None and mcm_inv is not None:
-        print ('Error: you have to choose between binned or unbinned mcm')
+        print ("Error: you have to choose between binned or raw mcm")
         sys.exit()
 
-    
     bin_lo,bin_hi,bin_c,bin_size= pspy_utils.read_binning_file(binning_file,lmax)
     n_bins=len(bin_hi)
     
-    if type=='Dl':
+    if type=="Dl":
         fac=(l*(l+1)/(2*np.pi))
-    if type=='Cl':
+    if type=="Cl":
         fac=l*0+1 
 
     if spectra is None:
@@ -114,10 +143,22 @@ def bin_spectra(l,cl,binning_file,lmax,type,spectra=None,mbb_inv=None,mcm_inv=No
     return(binCent,binnedPower)
 
 def vec2spec_dict(n_bins,vec,spectra):
+    
+    """Take a vector of power spectra and return a power spectra dictionnary.
+    vec should be of the form [spectra[0], spectra[1], ... ].
+    For example [cl_TT,cl_TE,cl_ET, ...., cl_BB]
+    
+    Parameters
+    ----------
+    n_bins: int
+      the number of bins per spectrum
+    vec: 1d array
+      an array containing a vector of spectra
+    spectra: list of strings
+      the arrangement of the spectra for example:
+      ['TT','TE','TB','ET','BT','EE','EB','BE','BB']
     """
-    @brief take a vector of power spectra and return a power spectra dictionnary
-    vec should be of the form [spectra[0], spectra[1], ... ]
-    """
+    
     dict={}
     for c,f in enumerate(spectra):
         dict[f]=vec[c*n_bins:(c+1)*n_bins]
@@ -125,12 +166,21 @@ def vec2spec_dict(n_bins,vec,spectra):
 
 
 def write_ps(file_name,l,ps,type,spectra=None):
-    """
-    @brief write down the power spectra
-    @param l the angular multipole
-    @param ps the power spectrum, if spectra is not None, expect a dictionary with entry spectra
-    @param type: can be 'Cl' or 'Dl'
-    @param (optional) spectra: needed for spin0 and spin2 cross correlation, the arrangement of the spectra
+    
+    """Write down the power spectra to disk.
+    
+    Parameters
+    ----------
+    file_name: str
+      the name of the file to write the spectra
+    l: 1d array
+      the multipoles (or binned multipoles)
+    ps: 1d array or dict of 1d array
+      the power spectrum, if spectra is not None, expect a dictionary with entry spectra
+    type:  string
+      'Cl' or 'Dl'
+    spectra: list of strings
+      needed for spin0 and spin2 cross correlation, the arrangement of the spectra
     """
 
     if spectra is None:
@@ -151,12 +201,23 @@ def write_ps(file_name,l,ps,type,spectra=None):
 
 
 def read_ps(file_name,spectra=None):
+    
+    """Read the power spectra.
+        
+    Parameters
+    ----------
+    file_name: str
+      the name of the file to read the spectra
+    spectra: list of strings
+      needed for spin0 and spin2 cross correlation, the arrangement of the spectra
+      
+    Return
+    ----------
+      
+    The function return the multipole l (or binned multipoles) and a 1d power spectrum
+    array or a dictionnary of power spectra (if spectra is not None).
     """
-    @brief read the power spectra
-    @param file_name: the name of the datafile
-    @param (optional) spectra: needed for spin0 and spin2 cross correlation, the arrangement of the spectra
-    @return l,ps
-    """
+    
     data=np.loadtxt(file_name)
     if spectra is None:
         return data[:,0],data[:,1]
@@ -170,13 +231,21 @@ def read_ps(file_name,spectra=None):
 
 
 def write_ps_hdf5(file,spec_name,l,ps,spectra=None):
-    """
-    @brief write down the power spectra in hdf5
-    @param file: the name of the hdf5 file
-    @param spec_name: the name of the group in the hdf5 file
-    @param l the angular multipole
-    @param ps the power spectrum, if spectra is not None, expect a dictionary with entry spectra
-    @param (optional) spectra: needed for spin0 and spin2 cross correlation, the arrangement of the spectra
+    
+    """Write down the power spectra in a hdf5 file.
+        
+    Parameters
+    ----------
+    file: hdf5
+      the name of the hdf5 file
+    spec_name: string
+      the name of the group in the hdf5 file
+    l: 1d array
+      the multipoles (or binned multipoles)
+    ps: 1d array or dict of 1d array
+      the power spectrum, if spectra is not None, expect a dictionary with entry spectra
+    spectra: list of strings
+      needed for spin0 and spin2 cross correlation, the arrangement of the spectra
     """
 
     def array_from_dict(l,ps,spectra=None):
@@ -195,11 +264,24 @@ def write_ps_hdf5(file,spec_name,l,ps,spectra=None):
 
 
 def read_ps_hdf5(file,spec_name,spectra=None):
-    """
-    @brief read the power spectra in a hdf5 file
-    @param file: the name of the hdf5 file
-    @param spec_name: the name of the group in the hdf5 file
-    @param (optional) spectra: needed for spin0 and spin2 cross correlation, the arrangement of the spectra
+
+    """Read the power spectra in a hdf5 file.
+        
+    Parameters
+    ----------
+    file: hdf5
+      the name of the hdf5 file
+    spec_name: string
+      the name of the group in the hdf5 file
+    spectra: list of strings
+      needed for spin0 and spin2 cross correlation, the arrangement of the spectra
+
+    Return
+    ----------
+
+    The function returns the multipole l (or binned multipoles) and a 1d power spectrum
+    array (or a dictionnary of power spectra if spectra is not None).
+
     """
     
     spec=file[spec_name]
